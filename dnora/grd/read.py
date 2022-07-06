e7e5cb90dcc507450559eff314db574f72fc5dd7
Jan-Victor Björkqvist <janvb@met.no>
2022-07-06 13:31:36 +0200

    Added Xyz-reader

--- dnora/grd/read.py
+++ dnora/grd/read.py
@@ -113,21 +113,16 @@ class EMODNET2018(TopoReader):
     def __str__(self):
         return(f"Reading EMODNET topography from {self.source}.")
 
-class KartverketNo50m(TopoReader):
-    """Reads data from Kartverket bathymetry.
 
-        High resolution bathymetry dataset for the whole Norwegian Coast.
-        Can be found at:
-        https://kartkatalog.geonorge.no/metadata/dybdedata-terrengmodeller-50-meters-grid-landsdekkende/bbd687d0-d34f-4d95-9e60-27e330e0f76e
+class Xyz(TopoReader):
+    """Reads data from an Xyz ascii file. Data can be in UTM or lon, lat
 
-        For reading several files at once, supply the 'tile' argument with a glob pattern, e.g. 'B*'.
+    Contributed by: https://github.com/emiliebyer
+    """
 
-        Contributed by: https://github.com/emiliebyer
-        """
-
-    def __init__(self, expansion_factor: float=1.2, utmzone: int=33,
-                 tile: str='B1008', folder: str='/lustre/storeB/project/fou/om/WW3/bathy/kartverket_50m_x_50m') -> Tuple:
-        self.source=f'{folder}/{tile}_grid50_utm33.xyz'
+    def __init__(self, expansion_factor: float=1.2, utmzone: int=None,
+                 filename: str, folder: str='/lustre/storeB/project/fou/om/WW3/bathy/kartverket_50m_x_50m') -> Tuple:
+        self.source=f'{folder}/{filename}'
         self.expansion_factor = expansion_factor
         self.utmzone = utmzone
 
@@ -148,8 +143,10 @@ class KartverketNo50m(TopoReader):
         z = np.array(df['z'].astype(float))
 
         # Converting from utm to latitude and longitude
-        lat, lon = utm.to_latlon(x, y, self.utmzone, northern=True, strict=False)
-
+        if self.utmzone is not None:
+            lat, lon = utm.to_latlon(x, y, self.utmzone, northern=True, strict=False)
+        else:
+            lat, lon = y, x
         # Applying given max and min values for lat and lon
         mask_lat = np.logical_and(lat0 <= lat, lat <= lat1)
         mask_lon = np.logical_and(lon0 <= lon, lon <= lon1)
@@ -192,6 +189,29 @@ class KartverketNo50m(TopoReader):
 
 
         return topo, topo_lon, topo_lat
+
+    def __str__(self):
+        return(f"Reading xyz topography data from {self.source}.")
+
+class KartverketNo50m(Xyz):
+    """Reads data from Kartverket bathymetry.
+
+        High resolution bathymetry dataset for the whole Norwegian Coast.
+        Can be found at:
+        https://kartkatalog.geonorge.no/metadata/dybdedata-terrengmodeller-50-meters-grid-landsdekkende/bbd687d0-d34f-4d95-9e60-27e330e0f76e
+
+        For reading several files at once, supply the 'tile' argument with a glob pattern, e.g. 'B*'.
+
+        Contributed by: https://github.com/emiliebyer
+        """
+
+    def __init__(self, expansion_factor: float=1.2, utmzone: int=33,
+                 tile: str='B1008', folder: str='/lustre/storeB/project/fou/om/WW3/bathy/kartverket_50m_x_50m') -> Tuple:
+        self.source=f'{folder}/{tile}_grid50_utm33.xyz'
+        self.expansion_factor = expansion_factor
+        self.utmzone = utmzone
+
+        return
 
     def __str__(self):
         return(f"Reading Kartverket topography from {self.source}.")
